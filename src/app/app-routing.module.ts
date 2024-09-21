67e0e97b4c213097e0bf82a1c8ab80ddeb71fe4b
waleedHAlmenawy <waleed.almenawy@outlook.com>
2024-09-21 10:39:21 +0300

    feat(button): add navigation to flight results page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { FlightResultComponent } from './components/flight-result/flight-result.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent}
+  { path: '', component: HomeComponent },
+  { path: 'flight-results', component: FlightResultComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
